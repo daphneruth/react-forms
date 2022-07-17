8999afb22034f5317e62271c0fb30530b2c3a357
Daphne <daphneruth304@gmail.com>
2022-07-17 16:45:03 +0300

    submit on keystroke

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,16 @@
 import { useRef,useState } from "react";
 const SimpleInput = (props) => {
-const [enteredName, setEnteredName] = useState('')
+const [enteredName, setEnteredName] = useState('');
+
+const nameInputHandler = event=>{
+  setEnteredName(event.target.value);
+};
 
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
