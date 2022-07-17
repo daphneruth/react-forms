2dfe18e416a419817a5ceb27cafcf615651d9ae2
Daphne <daphneruth304@gmail.com>
2022-07-17 22:45:43 +0300

    using custom hooks

---
+++ src/hooks/use-input.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+const useInput = ()=>{
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isTouched, setIsTouched]= useState(false);
+   
+}
+export default useInput;
