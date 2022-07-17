647c0cb9b3fcff062ee1f96da8148c77f969ce07
Daphne <daphneruth304@gmail.com>
2022-07-17 16:51:46 +0300

    using useRef hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,7 +1,7 @@
 import { useRef,useState } from "react";
 const SimpleInput = (props) => {
 const [enteredName, setEnteredName] = useState('');
-
+const nameInputRef = useRef();
 const nameInputHandler = event=>{
   setEnteredName(event.target.value);
 };
@@ -13,7 +13,7 @@ const nameInputHandler = event=>{
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputHandler}/>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
