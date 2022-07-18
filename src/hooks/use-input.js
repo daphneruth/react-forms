d6ace757e219225c2575876c95048d8c96b0c682
Daphne <daphneruth304@gmail.com>
2022-07-18 06:12:15 +0300

    updated

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 const useInput = ()=>{
     const [enteredValue, setEnteredValue] = useState('');
     const [isTouched, setIsTouched]= useState(false);
-   
+
+    const valueIsValid = validValue(enteredValue);
+      const hasError = !valueIsValid && isTouched;
+
 }
 export default useInput;
